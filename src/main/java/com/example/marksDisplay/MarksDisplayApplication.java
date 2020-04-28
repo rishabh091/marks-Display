95be373606916b82eec7c6aa8d93be4b0f832d25
Rishabh <rishabhmalhotra091@gmail.com>
2020-04-29 03:28:32 +0530

    final commit

--- src/main/java/com/example/marksDisplay/MarksDisplayApplication.java
+++ src/main/java/com/example/marksDisplay/MarksDisplayApplication.java
@@ -10,7 +10,6 @@ public class MarksDisplayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(MarksDisplayApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port","8081"));
 		app.run(args);
 	}
 
